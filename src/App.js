5a73d71f4d26e0098300eb47b9ddf5b076d5ce01
benderpan <272569492@qq.com>
2019-05-03 19:05:19 +0800

    Arwes test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
+
+import { ThemeProvider, createTheme, Arwes, Heading, Button, Line } from '@arwes/arwes';
+import GraphItem from './GraphItem'
 import './App.css';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <ThemeProvider theme={createTheme()}>
+      <Arwes>
+        <div style={{ margin: 20 }}>
+          <Heading node='h5'>Bot V1.0</Heading>
+          <div style={{ padding: '5px' }}>
+            <Button animate disabled>FuncA</Button>
+            {' '}
+            <Button animate>FuncB</Button>
+            {' '}
+            <Button animate layer='success'>
+              <i className='mdi mdi-chemical-weapon' /> FuncC
+            </Button>
+            {' '}
+            <Button animate layer='alert'>
+              FuncD <i className='mdi mdi-robot' />
+            </Button>
+          </div>
+          <div>
+            <GraphItem ></GraphItem>
+            <Line animate />
+            <p>Processing...</p>
+            <Line animate layer='success' />
+            <p>1.Eating...</p>
+            <p>2.Walking...</p>
+            <p>3.Charging...</p>
+            <p>4.Sleeping...</p>
+            <Line animate layer='alert' />
+          </div>
+        </div>
+      </Arwes>
+    </ThemeProvider>
   );
 }
-
 export default App;
