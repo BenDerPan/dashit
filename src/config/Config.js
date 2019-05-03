ed74f77d0f0d87707d5342ef491b5520d29e1227
benderpan <benderpan@163.com>
2019-05-03 18:47:05 +0800

    Init

---
+++ src/config/Config.js
@@ -0,0 +1,7 @@
+const ClientWSAddress="ws://localhost:9870/stream?token=CiqNunYzeH-Vku2";
+const AppPushAddress="http://localhost:9870/message?token=A5LbvQPy4xAYX0N"
+const Config={}
+Config.WSAddress=ClientWSAddress;
+Config.PushAddress=AppPushAddress;
+
+export default Config;
