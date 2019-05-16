807c166c3845bb8ecd96161435c47511378841bb
benderpan <272569492@qq.com>
2019-05-16 19:22:57 +0800

    拖拉完成

--- src/AddRemoveLayout.js
+++ src/AddRemoveLayout.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { ThemeProvider, createTheme, Arwes, Heading, Button, Line, Row, Col, Project, Frame, Words } from '@arwes/arwes';
 import { WidthProvider, Responsive } from "react-grid-layout";
-import Icon from '@mdi/react'
-import { mdiAccount, mdiAirplay } from '@mdi/js'
+import GraphItem from './components/GraphItem'
+import '@mdi/font/css/materialdesignicons.css'
 
 import _ from "lodash";
 
@@ -12,52 +12,33 @@ import 'react-grid-layout/css/styles.css'
 import 'react-resizable/css/styles.css'
 
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
+// saveItemsToLS("items",[]);
+// saveLayoutsToLS("layouts",{})
+const originalLayouts = getLayoutsFromLS("layouts") || {};
+const originalItems=getItemsFromLS("items") || [];
+const ClientUnitWidth=Math.ceil(document.getElementById('root').clientWidth / 12)-10;
 
 class AddRemoveLayout extends React.PureComponent {
   static defaultProps = {
+    className: "layout",
     cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
     rowHeight: 100,
-    onLayoutChange: () => { }
   };
 
   constructor(props) {
     super(props);
-
+  
     this.state = {
-      items: [
-        {
-          index: 0,
-          i: "GraphView1",
-          x: 0,
-          y: 0,
-          w: 3,
-          h: 3,
-          frameHeight: { height: 3 * AddRemoveLayout.defaultProps.rowHeight + 20 },
-        },
-        {
-          index: 1,
-          i: "GraphView2",
-          x: 2,
-          y: 0,
-          w: 3,
-          h: 3,
-          frameHeight: { height: 3 * AddRemoveLayout.defaultProps.rowHeight + 20 },
-        },
-        {
-          index: 2,
-          i: "GraphView3",
-          x: 4,
-          y: 0,
-          w: 3,
-          h: 3,
-          frameHeight: { height: 3 * AddRemoveLayout.defaultProps.rowHeight + 20 },
-        },
-      ],
-      newCounter: 4
+      layouts: JSON.parse(JSON.stringify(originalLayouts)),
+      items: JSON.parse(JSON.stringify(originalItems)),
+      
+      newCounter: 0
     };
-    console.log("初始化:",this.state.items);
+    console.log("初始化:", this.state.items);
     this.onAddItem = this.onAddItem.bind(this);
+    this.onResetAll=this.onResetAll.bind(this);
     this.onBreakpointChange = this.onBreakpointChange.bind(this);
+    console.log(this.state.layouts,this.state.items);
   }
 
   createElement(el) {
@@ -67,41 +48,64 @@ class AddRemoveLayout extends React.PureComponent {
       top: 0,
       cursor: "pointer",
     };
+    switch(el.componentType){
+      case "table":
+
+      break;
+
+      default:
+      break;
+
+    }
     return (
       <div key={el.i} data-grid={el}>
         <Frame style={el.frameHeight}>
-          <Words>{el.i}</Words>
-          <span style={removeStyle} onClick={this.onRemoveItem.bind(this, el.i)}>Close</span>
-          <Line></Line>
+          <div>
+            <Words>{"["+el.index+"]" + el.title}</Words>
+            <span style={removeStyle} onClick={this.onRemoveItem.bind(this, el.i)}>Close</span>
+            <Line style={{margin:0,padding:0}}></Line>
+          </div>
+          <div id={el.i} style={{margin:0,}}>
+            <GraphItem parentWidth={el.frameWidth} parentHeight={el.frameHeight.height - 52}></GraphItem>
+          </div>
         </Frame>
-
       </div>
     );
   }
 
-  onAddItem() {
+  onAddItem(componentType) {
     /*eslint no-console: 0*/
     console.log("adding", this.state.newCounter);
-    let keyName="GraphView"+this.state.newCounter.toString();
+
+    let keyName =new Date().getTime().toString();
     const items = [...this.state.items,
-      {
-        index:this.state.newCounter,
-        i: keyName,
-        x: (this.state.items.length * 2) % (this.state.cols || 12),
-        y: Infinity, // puts it at the bottom
-        w: 3,
-        h: 3,
-        frameHeight: { height: 3 * AddRemoveLayout.defaultProps.rowHeight + 20 },
-      }
+    {
+      index: this.state.newCounter,
+      i: keyName,
+      x: (this.state.items.length * 2) % (this.state.cols || 12),
+      y: this.state.newCounter/4, // puts it at the bottom
+      w: 3,
+      h: 3,
+      type:componentType,
+      title:"Monitor"+this.state.newCounter.toString(),
+      frameHeight: { height: 3 * AddRemoveLayout.defaultProps.rowHeight+(3-1)*10},
+      frameWidth:ClientUnitWidth*3,
+    }
     ];
-    
-    console.log("新增后：",items,items.length);
+    //重新排序Index
+    for(let n=0;n<items.length;n++){
+      items[n].index=n;
+      items[n].title="Monitor"+n.toString();
+      items[n].i="M"+n.toString();
+    }
+    console.log("新增后：", items, items.length);
     this.setState({
       // Add a new item. It must have a unique key!
-      items:items,
+      items: items,
       // Increment the counter to ensure key is always unique.
       newCounter: this.state.newCounter + 1
     });
+    saveItemsToLS("items",items);
   }
 
   // We're using the cols coming back from this to calculate where to add new items.
@@ -112,27 +116,30 @@ class AddRemoveLayout extends React.PureComponent {
     });
   }
 
-  onLayoutChange(layout) {
-    this.props.onLayoutChange(layout);
-    this.setState({ layout: layout });
+  onLayoutChange(layout,layouts) {
+    saveLayoutsToLS("layouts", layouts);
+    //this.props.onLayoutChange(layout);
+    this.setState({ layouts: layouts });
+    saveItemsToLS("items",this.state.items);
   }
 
   onRemoveItem(i) {
     console.log("removing", i);
-    const items=[...this.state.items.filter((s)=>{return s.i!=i})];
-    console.log("删除后：",items);
+    const items = [...this.state.items.filter((s) => { return s.i != i })];
+    console.log("删除后：", items);
 
-    this.setState({ items:items});
+    this.setState({ items: items });
+    saveItemsToLS("items",items);
   }
 
   onResize(layout, oldLayoutItem, layoutItem, placeholder) {
-    // `oldLayoutItem` contains the state of the item before the resize.
-    // You can modify `layoutItem` to enforce constraints.
-    // const items = [...this.state.items];
-    const items=[...this.state.items];
-    let index=items.findIndex((s)=>{return s.i===layoutItem.i});
-    if(index>-1){
-      items[index].frameHeight = { height: layoutItem.h * this.props.rowHeight + (layoutItem.h - 1) * 10 }
+    const items = [...this.state.items];
+    console.log("resize");
+    let w=document.getElementById(layoutItem.i).clientWidth;
+    let index = items.findIndex((s) => { return s.i === layoutItem.i });
+    if (index > -1) {
+      items[index].frameHeight = { height: layoutItem.h * this.props.rowHeight+(layoutItem.h-1)*10};
+      items[index].frameWidth=w;//ClientUnitWidth*layoutItem.w;
     }
     this.setState({
       items: items
@@ -144,28 +151,55 @@ class AddRemoveLayout extends React.PureComponent {
     return (
       <div>
         <div style={{ margin: 5 }}>
-          <Button onClick={this.onAddItem}>
-            <Icon path={mdiAccount}
-              size={0.5}
-              horizontal
-              vertical
-              rotate={90}
-              color="green"
-              spin />
+        <Button onClick={this.onResetAll} animate layer='alert'>
+            <i className='mdi mdi-lock-reset'></i>
           </Button>
-          <Button onClick={this.onAddItem}>
-            <Icon path={mdiAirplay}
-              size={0.5}
-              horizontal
-              vertical
-              rotate={90}
-              color="red"
-              spin />
+          {' '}
+          <Button onClick={()=>this.onAddItem("table")}>
+            <i className='mdi mdi-grid'></i>
+          </Button>
+          {' '}
+          <Button onClick={()=>this.onAddItem("pie")}>
+            <i className='mdi mdi-chart-pie'></i>
+          </Button>
+          {' '}
+          <Button onClick={()=>this.onAddItem("line")}>
+            <i className='mdi mdi-chart-line'></i>
+
+          </Button>
+          {' '}
+          <Button onClick={()=>this.onAddItem("bar")}>
+            <i className='mdi mdi-chart-bar'></i>
+          </Button>
+          {' '}
+          <Button onClick={()=>this.onAddItem("list")}>
+            <i className='mdi mdi-format-list-bulleted'></i>
+          </Button>
+          {' '}
+          <Button onClick={()=>this.onAddItem("text")}>
+            <i className='mdi mdi-tag-text-outline'></i>
+          </Button>
+          {' '}
+          <Button onClick={()=>this.onAddItem("graph")}>
+            <i className='mdi mdi-hubspot'></i>
+          </Button>
+          {' '}
+          <Button onClick={()=>this.onAddItem("image")}>
+            <i className='mdi mdi-tooltip-image'></i>
+          </Button>
+          {' '}
+          <Button onClick={()=>this.onAddItem("settings")}>
+            <i className='mdi mdi-tune'></i>
+          </Button>
+          {' '}
+          <Button onClick={()=>this.onAddItem("network_status")}>
+            <i className='mdi mdi-lan-connect'></i>
           </Button>
         </div>
         <Line></Line>
         <ResponsiveReactGridLayout onResize={this.onResize.bind(this)}
-          onLayoutChange={this.onLayoutChange}
+          layouts={this.state.layouts}
+          onLayoutChange={(layout,layouts)=>this.onLayoutChange(layout,layouts)}
           onBreakpointChange={this.onBreakpointChange}
           {...this.props}
         >
@@ -174,7 +208,67 @@ class AddRemoveLayout extends React.PureComponent {
       </div>
     );
   }
+
+  onResetAll(){
+    this.setState({
+      items:[],
+      layouts:{},
+    });
+    saveItemsToLS("items",this.state.items);
+    saveLayoutsToLS("layouts",this.state.layouts);
+  };
 }
+
+
+
+function getLayoutsFromLS(key) {
+  let ls = {};
+  if (global.localStorage) {
+    try {
+      ls = JSON.parse(global.localStorage.getItem("DASHIT-LAYOUTS")) || {};
+    } catch (e) {
+      /*Ignore*/
+    }
+  }
+  return ls[key];
+}
+
+function saveLayoutsToLS(key, value) {
+  if (global.localStorage) {
+    global.localStorage.setItem(
+      "DASHIT-LAYOUTS",
+      JSON.stringify({
+        [key]: value
+      })
+    );
+  }
+}
+function getItemsFromLS(key) {
+  let ls = [];
+  if (global.localStorage) {
+    try {
+      ls = JSON.parse(global.localStorage.getItem("DASHIT-ITEMS")) || [];
+    } catch (e) {
+      /*Ignore*/
+    }
+  }
+  return ls[key];
+}
+
+function saveItemsToLS(key, value) {
+  console.log("Save Items",JSON.stringify({
+    [key]: value
+  }));
+  if (global.localStorage) {
+    global.localStorage.setItem(
+      "DASHIT-ITEMS",
+      JSON.stringify({
+        [key]: value
+      })
+    );
+  }
+}
+
 
 export default AddRemoveLayout;
 
--- src/components/GraphItem.js
+++ src/components/GraphItem.js
@@ -33,14 +33,14 @@ class GraphItem extends Component {
 
     render() {
         return (
-            <ForceGraph3D width={this.props.parentWidth}  height={this.props.parentHeight}
+            <ForceGraph3D style={{padding:10}} width={this.props.parentWidth}  height={this.props.parentHeight}
                 graphData={this.state.datas}
                 nodeAutoColorBy="group"
                 linkColor="#F8B209"
                 nodeThreeObject={node => {
                     const sprite = new SpriteText(node.id);
                     sprite.color = node.color;
-                    //sprite.textHeight = 8;
+                    sprite.textHeight = 8;
                     return sprite;
                 }}
             />
